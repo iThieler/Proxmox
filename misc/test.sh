ebebff4788ec758ef1bab22c73c8154d75d58bc8
iThieler <39486895+iThieler@users.noreply.github.com>
2022-11-05 07:47:29 +0100

    Create test.sh

---
+++ misc/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -n "$1" ]; then
+  configFile="$1"
+fi
+fileName=$(basename "${BASH_SOURCE:-$0}")
+filePATH=$(realpath "$0" | sed 's|\(.*\)/.*|\1|')
+
+echo "The Filepath is ${filePATH}"
+echo "----------------------------------"
+echo "The FileName is ${fileName}"
+echo "----------------------------------"
+echo "The configFile is ${configFile}"
