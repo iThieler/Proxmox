e702283e3464a306811b2ae9a80a272d815e3bac
iThieler <39486895+iThieler@users.noreply.github.com>
2022-11-05 07:37:51 +0100

    Update global-config-file.sh

--- misc/global-config-file.sh
+++ misc/global-config-file.sh
@@ -2,11 +2,11 @@
 
 echo "$1"
 
-fileName=$(basename "${BASH_SOURCE:-$0}")
-filePATH=$(realpath "$0" | sed 's|\(.*\)/.*|\1|')
+#fileName=$(basename "${BASH_SOURCE:-$0}")
+#filePATH=$(realpath "$0" | sed 's|\(.*\)/.*|\1|')
 
-echo "The Filepath is ${filePATH}"
-echo "----------------------------------"
-echo "The FileName is ${fileName}"
-echo "----------------------------------"
-echo "The configFile is ${configFile}"
+#echo "The Filepath is ${filePATH}"
+#echo "----------------------------------"
+#echo "The FileName is ${fileName}"
+#echo "----------------------------------"
+#echo "The configFile is ${configFile}"
