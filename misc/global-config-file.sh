d21de504ccff6d5695e49f5b03a4754cbf2d6ce4
iThieler <39486895+iThieler@users.noreply.github.com>
2022-11-05 07:14:43 +0100

    Update global-config-file.sh

--- misc/global-config-file.sh
+++ misc/global-config-file.sh
@@ -5,7 +5,7 @@ if [ -n "${1}" ]; then
 fi
 
 fileName=$(basename "${BASH_SOURCE:-$0}")
-filePATH=$(realpath "$0" | sed "s/\/$fileNAME//")
+filePATH=$(realpath "$0")
 
 echo "The Filepath is ${filePATH}"
 echo "----------------------------------"
