3ebc2a5fcbed9f4db98a279c7c009ad1c0f0084d
iThieler <39486895+iThieler@users.noreply.github.com>
2022-11-09 14:15:05 +0100

    Update checkup.sh

--- checkup.sh
+++ checkup.sh
@@ -22,6 +22,58 @@ function cloneGIT() {
   #done 
 }
 
+function menuMAIN() {
+  sel=("1" "... update my HomeServer" \
+       "2" "... update my HomeServer and all containers" \
+       "3" "... install and configure containers" \
+       "4" "... install and configure virtual machine(s)" \
+       "5" "... create a backup of one or more guest systems" \
+       "6" "... restore one or more guest systems from backup" \
+       "7" "... delete one or more containers" \
+       "8" "... delete one or more virtual machine(s)" \
+       "" "" \
+       "Q" "... exit and clean up"
+  menuSelection=$(whiptail --menu --nocancel --backtitle "© 2021 - SmartHome-IoT.net" --title " CONFIGURING PROXMOX " "\nWhat do you want to do?" 20 80 10 "${sel[@]}" 3>&1 1>&2 2>&3)
+
+  if [[ $menuSelection == "1" ]]; then
+    update "server"
+    menuMAIN
+  elif [[ $menuSelection == "2" ]]; then
+    update "all"
+    menuMAIN
+  elif [[ $menuSelection == "3" ]]; then
+    install "LXC"
+    menuMAIN
+  elif [[ $menuSelection == "4" ]]; then
+    install "VM"
+    menuMAIN
+  elif [[ $menuSelection == "5" ]]; then
+    whiptail --yesno --yes-button " ${btn_11} " --no-button " ${btn_12} " --backtitle "© 2021 - SmartHome-IoT.net" --title " ${tit_0008} " "\n${txt_0038}?" 10 80
+    yesno=$?
+    if [ $yesno -eq 0 ]; then
+      backuprestore "backup" "all"
+    else
+      backuprestore "backup" "select"
+    fi
+    menuMAIN
+  elif [[ $menuSelection == "6" ]]; then
+    backuprestore "restore" "all"
+    menuMAIN
+  elif [[ $menuSelection == "7" ]]; then
+    delete "LXC"
+    menu
+  elif [[ $menuSelection == "8" ]]; then
+    delete "VM"
+    menuMAIN
+  elif [[ $menuSelection == "Q" ]]; then
+    finish
+    exit 0
+  else
+    menuMAIN
+  fi
+}
+}
+
 # Check Proxmox
 if ! command -v pveversion >/dev/null 2>&1; then
   NEWT_COLORS='
@@ -64,6 +116,7 @@ fi
 if [ -f "/root/.iThieler" ]; then
   birth=$(stat .iThieler | grep "Birth" | cut -d' ' -f3,4,5)
   echo -e "$(date +'%Y-%m-%d  %T')  [\033[1;31mERROR\033[0m]  Configuration almost done at >> ${birth}"
+  menuMAIN
 else
   cloneGIT "${gitREPONAME}"
   cd "/root/${gitREPONAME}"
