e94d63ebdc739d736431f90637fb181eeca2829a
iThieler <39486895+iThieler@users.noreply.github.com>
2022-11-10 16:13:45 +0100

    Update config-pve.sh

--- misc/config-pve.sh
+++ misc/config-pve.sh
@@ -47,19 +47,4 @@ echo -e "[OPTIONS]\n \
 # create Backuppool
 pvesh create /pools --poolid BackupPool --comment "VMs in this pool are backed up daily"
 
-# configure sources.lists
-sed -i "s/^deb/#deb/g" /etc/apt/sources.list.d/pve-enterprise.list
-cat <<EOF > /etc/apt/sources.list
-deb http://ftp.debian.org/debian bullseye main contrib
-deb http://ftp.debian.org/debian bullseye-updates main contrib
-deb http://security.debian.org/debian-security bullseye-security main contrib
-EOF
-cat <<EOF >> /etc/apt/sources.list.d/pve-no-subscription.list
-deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription
-EOF
-echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/data.status/{s/\!//;s/Active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" > /etc/apt/apt.conf.d/no-nag-script
-apt --reinstall install proxmox-widget-toolkit &>/dev/null
-
-updateHost
-
 whip_message "PROXMOX" "Die Grundkonfiguration des Servers ist nun abgeschlossen. Der Server muss neu gestartet werden. Nach dem Neustart kann dieses Skript zur installation und konfiguration von Containern und virtuellen Maschinen erneut aufgerufen werden."
