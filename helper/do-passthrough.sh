93f4d8418462636d5f4c74a6d7412ae6083c5bbb
iThieler <39486895+iThieler@users.noreply.github.com>
2024-06-09 14:28:01 +0200

    Update do-passthrough.sh
    Complete revision of the ZigBee function

--- helper/do-passthrough.sh
+++ helper/do-passthrough.sh
@@ -8,7 +8,7 @@ function zigbee() {
     #get container ID and stop
     ctID=$(whip_inputbox "OK" "Container ID" "Wie lautet die Container-ID, an die der Zigbee-Stick gebunden werden soll?" "$(pct list | grep "ioBroker" | cut -d' ' -f 1)")
     pct stop $ctID
-    while [ $(pct status $ctID | cut -d' ' -f2 | grep -cw running) -eq 1 ]; do
+    while [ "$(pct status $ctID | cut -d' ' -f2 | grep -cw running)" -eq 1 ]; do
       sleep 2
     done
 
@@ -21,31 +21,46 @@ function zigbee() {
     fi
 
     #get device infos
-    device=$(lsusb | grep "$usbdev")
-    usbBUS=$(lsusb | grep "$usbdev" | cut -d' ' -f2)
-    usbDEVICE=$(lsusb | grep "$usbdev" | cut -d' ' -f4 | cut -d: -f1)
-    usbDEVICEID=$(ls -l /dev/bus/usb/$usbBUS/$usbDEVICE | cut -d' ' -f5 | cut -d, -f1)
-    usbVENDORID=$(lsusb | grep "$usbdev" | cut -d' ' -f6 | cut -d: -f1)
-    usbPRODUCTID=$(lsusb | grep "$usbdev" | cut -d' ' -f6 | cut -d: -f2)
-    usbPRODUCTNAME=$(ls /dev/serial/by-id/)
-    if [ -e "/dev/ttyACM"* ]; then
-      usbPATH=$(ls -l /dev/ttyACM* | cut -d' ' -f10 | cut -d/ -f3)
-      usbDAILOUT=$(ls -l /dev/ttyACM* | cut -d' ' -f5 | cut -d, -f1)
-    elif [ -e "/dev/ttyUSB"* ]; then
-      usbPATH=$(ls -l /dev/ttyUSB* | cut -d' ' -f10 | cut -d/ -f3)
-      usbDAILOUT=$(ls -l /dev/ttyUSB* | cut -d' ' -f5 | cut -d, -f1)
+    usbdev=""
+    conbee=""
+
+    if [[ $(lsusb | grep -cw "ConBee II") -eq 1 ]]; then
+      usbdev="ConBee II"
+      conbee=true
+    elif [[ $(lsusb | grep -cw "Silicon Labs") -eq 1 ]]; then
+      usbdev="Silicon Labs"
+      conbee=false
     fi
 
-    if [[ $(lsusb | grep "$usbdev" | grep -cw "ConBee II") -eq 1 ]]; then
-      mkdir /var/lib/lxc/$ctID/devices
-      cd /var/lib/lxc/$ctID/devices
-      mknod -m 666 $usbPATH c $usbDAILOUT 0
-      cd /root
+    if [ -z "$usbdev" ]; then
+      echoLOG r "Kein unterstütztes USB-Gerät gefunden."
+      exit 1
+    fi
+
+    device=$(lsusb | grep "$usbdev")
+    usbBUS=$(echo "$device" | cut -d' ' -f2)
+    usbDEVICE=$(echo "$device" | cut -d' ' -f4 | cut -d: -f1)
+    usbDEVICEID=$(ls -l "/dev/bus/usb/$usbBUS/$usbDEVICE" | awk '{print $5}' | cut -d, -f1)
+    usbVENDORID=$(echo "$device" | awk '{print $6}' | cut -d: -f1)
+    usbPRODUCTID=$(echo "$device" | awk '{print $6}' | cut -d: -f2)
+    usbPRODUCTNAME=$(ls /dev/serial/by-id/ | head -n 1)
+
+    if [ -e "/dev/ttyACM"* ]; then
+      usbPATH=$(ls -l /dev/ttyACM* | awk '{print $10}' | cut -d/ -f3)
+      usbDAILOUT=$(ls -l /dev/ttyACM* | awk '{print $5}' | cut -d, -f1)
+    elif [ -e "/dev/ttyUSB"* ]; then
+      usbPATH=$(ls -l /dev/ttyUSB* | awk '{print $10}' | cut -d/ -f3)
+      usbDAILOUT=$(ls -l /dev/ttyUSB* | awk '{print $5}' | cut -d, -f1)
+    fi
+
+    if $conbee; then
+      mkdir -p "/var/lib/lxc/$ctID/devices"
+      mknod -m 666 "/var/lib/lxc/$ctID/devices/$usbPATH" c "$usbDAILOUT" 0
       line1="lxc.cgroup2.devices.allow: c $usbDEVICEID:* rwm"
       line2="lxc.mount.entry: $usbPRODUCTNAME dev/serial/by-id/$usbPRODUCTNAME none bind,optional,create=file"
       line3="lxc.cgroup2.devices.allow: c $usbDAILOUT:* rwm"
       line4="lxc.mount.entry: /var/lib/lxc/$ctID/devices/$usbPATH dev/$usbPATH none bind,optional,create=file"
-    elif [[ $(lsusb | grep "$usbdev" | grep -cw "Silicon Labs") -eq 1 ]]; then
+    else
       line1="lxc.cgroup2.devices.allow: c $usbDEVICEID:* rwm"
       line2="lxc.mount.entry: $usbPRODUCTNAME dev/serial/by-id/$usbPRODUCTNAME none bind,optional,create=file"
       line3="lxc.cgroup2.devices.allow: c $usbDAILOUT:* rwm"
@@ -53,19 +68,19 @@ function zigbee() {
     fi
 
     #search for line number to insert if there are deviations in the config file
-    if [[ $(cat "/etc/pve/lxc/$ctID.conf" | grep -cw "\[") -eq 1 ]]; then
-      lineNUMBER=$(cat "/etc/pve/lxc/$ctID.conf" | grep -m1 -n "\[" | cut -d: -f1)
-      lineNUMBER=$(($lineNUMBER-1))
-      #insert text to lxc conigfile
+    if [[ $(grep -cw "\[" "/etc/pve/lxc/$ctID.conf") -eq 1 ]]; then
+      lineNUMBER=$(grep -m1 -n "\[" "/etc/pve/lxc/$ctID.conf" | cut -d: -f1)
+      lineNUMBER=$((lineNUMBER - 1))
+      #insert text to lxc config file
       sed -i "$lineNUMBER i $line1\n$line2\n$line3\n$line4" "/etc/pve/lxc/$ctID.conf"
     else
-      #insert text to lxc conigfile
+      #insert text to lxc config file
       echo -e "$line1\n$line2\n$line3\n$line4" >> "/etc/pve/lxc/$ctID.conf"
     fi
     chmod o+rw "/dev/$usbPATH"
 
     #create udev rule
-    echo -e "SUBSYSTEMS=="usb", ATTRS{idVendor}=="${usbVENDORID}", ATTRS{idProduct}=="${usbPRODUCTID}", GROUP="users", MODE="0666"" > "/etc/udev/rules.d/50-myusb.rules"
+    echo -e "SUBSYSTEMS==\"usb\", ATTRS{idVendor}==\"${usbVENDORID}\", ATTRS{idProduct}==\"${usbPRODUCTID}\", GROUP=\"users\", MODE=\"0666\"" > "/etc/udev/rules.d/50-myusb.rules"
     udevadm control --reload
 
     #start container
